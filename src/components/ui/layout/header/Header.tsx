c78b790df29f9676f722cac4dd2df5df1b3cef68
farneser <farneser1@gmail.com>
2023-10-13 17:28:41 +0300

    feat: header and product item use context

--- src/components/ui/layout/header/Header.tsx
+++ src/components/ui/layout/header/Header.tsx
@@ -1,27 +1,16 @@
-import {FC, useEffect, useState} from "react";
+import React, {useContext} from 'react';
+import {CartContext} from "../../../providers/CartProvider";
 import HeaderButton from "./button/HeaderButton";
-import {getCartItems} from "../../../../services/cartService";
-import {CartProduct} from "../../../../interfaces/CartProps";
 
-const Header: FC = () => {
-    const [cart, setCart] = useState<CartProduct[]>([]); // Initialize the cart state with an empty array
-
-    useEffect(() => {
-        // Fetch cart items when the component mounts
-        const fetchCartItems = async () => {
-            const cartItems = await getCartItems();
-            setCart(cartItems);
-        };
-
-        fetchCartItems();
-    }, []); // The empty dependency array ensures this effect runs only once on mount
+const Header: React.FC = () => {
+    const context = useContext(CartContext);
 
     return (
         <div id="header">
             <HeaderButton button_text={"cart"} path={"cart"}/>
             <HeaderButton button_text={"home"} path={"/"}/>
             <div>
-                <span>{cart.length}</span>
+                <span>{context ? context.cart.length : 0}</span>
             </div>
         </div>
     );
--- src/components/ui/product/item/ProductItem.tsx
+++ src/components/ui/product/item/ProductItem.tsx
@@ -1,30 +1,26 @@
-import React, {useState, useEffect, FC} from 'react';
+import React, {useState, useEffect, FC, useContext} from 'react';
 import {Product} from "../../../../interfaces/Product";
 import {addToCart, getCartItems} from "../../../../services/cartService";
+import {CartContext} from "../../../providers/CartProvider";
+import {CartProps} from "../../../../interfaces/CartProps";
 
 const ProductItem: FC<{ product: Product }> = ({product}) => {
 
-    const cart = getCartItems();
+    const context = useContext(CartContext);
+
     const [inCart, setInCart] = useState<boolean>(false);
 
-    const isProductInCart = (product: Product) => {
-        return cart.some(item => item.product.id === product.id);
-    };
-
     useEffect(() => {
-        setInCart(isProductInCart(product));
+        if (context) {
+            setInCart(context.isProductInCart(product));
+        }
     });
-
-    const handleAddToCart = (product: Product) => {
-        addToCart(product);
-        setInCart(true)
-    };
 
     return (
         <li>
             {product.name} - ${product.price}
             {inCart ? <a href={"cart"}>Go to Cart</a> :
-                <button onClick={() => handleAddToCart(product)}>Add to Cart</button>}
+                <button onClick={() => context && context.handleAddToCart(product)}>Add to Cart</button>}
         </li>
     );
 };
