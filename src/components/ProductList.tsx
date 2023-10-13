af98b7b1ea7b467babe8322c575f2d508cbc8894
farneser <farneser1@gmail.com>
2023-10-13 03:26:07 +0300

    feat: product list

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-
-interface Product {
-    id: number;
-    name: string;
-    price: number;
-}
+import React, {useState} from 'react';
+import {Product} from "../interfaces/Product";
+import {getCartItems} from "../services/cartService";
+import ProductItem from "./ProductItem";
 
 interface ProductListProps {
     products: Product[];
     addToCart: (product: Product) => void;
 }
 
-const ProductList: React.FC<ProductListProps> = ({ products, addToCart }) => {
+const ProductList: React.FC<ProductListProps> = ({products, addToCart}) => {
     return (
         <div>
             <h2>Products</h2>
             <ul>
                 {products.map((product) => (
-                    <li key={product.id}>
-                        {product.name} - ${product.price}
-                        <button onClick={() => addToCart(product)}>Add to Cart</button>
-                    </li>
+                    <ProductItem product={product}/>
                 ))}
             </ul>
         </div>
