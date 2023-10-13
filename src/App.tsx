58e6108fb90bf52470ada789c3d1b247e928e7e6
farneser <farneser1@gmail.com>
2023-10-13 17:29:30 +0300

    feat: app routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,25 @@
-import React, {useEffect, useState} from 'react';
-import ProductList from './components/ProductList';
-import Cart from './interfaces/Cart';
-import {Product} from "./interfaces/Product";
+import {
+    Route,
+    RouterProvider,
+    createBrowserRouter,
+    createRoutesFromElements,
+} from "react-router-dom";
+import HomePage from "./components/pages/HomePage";
+import CartPage from "./components/pages/CartPage";
+import React from "react";
+import Layout from "./components/ui/layout/Layout";
 
-function getProducts() {
-    return [
-        {id: 1, name: 'Product 1', price: 10.0},
-        {id: 2, name: 'Product 2', price: 15.0},
-        {id: 3, name: 'Product 3', price: 20.0},
-    ];
-}
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<Layout/>}>
+            <Route index element={<HomePage/>}/>
+            <Route path="/cart" element={<CartPage/>}/>
+        </Route>
+    )
+);
 
 function App() {
-    const initialProducts = getProducts()
-
-    const [products, setProducts] = useState<Product[]>(initialProducts);
-    const [cart, setCart] = useState<Product[]>([]);
-
-    useEffect(() => {
-        const savedCart = localStorage.getItem('cart');
-        if (savedCart) {
-            setCart(JSON.parse(savedCart));
-        }
-    }, []);
-
-    const addToCart = (product: Product) => {
-        const updatedCart = [...cart, product];
-        setCart(updatedCart);
-        localStorage.setItem('cart', JSON.stringify(updatedCart));
-    };
-
-    const removeFromCart = (product: Product) => {
-        const updatedCart = cart.filter((item) => item.id !== product.id);
-        setCart(updatedCart);
-        localStorage.setItem('cart', JSON.stringify(updatedCart));
-    };
-
-    return (
-        <div className="App">
-            <ProductList products={products} addToCart={addToCart}/>
-            <Cart cart={cart} removeFromCart={removeFromCart}/>
-        </div>
-    );
+    return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
