3e8b12a460903594e5b0f0be9926dd3285fedfe9
farneser <farneser1@gmail.com>
2023-10-14 11:22:29 +0300

    feat: header button styles

--- src/components/ui/layout/header/button/HeaderButton.tsx
+++ src/components/ui/layout/header/button/HeaderButton.tsx
@@ -1,31 +1,30 @@
-import {useNavigate} from "react-router-dom";
-import "./headerButton.scss";
+import React from 'react';
+import {useNavigate} from 'react-router-dom';
+import './headerButton.scss';
+import locations from '../../../../../locations';
 
 interface Props {
     button_text: string;
     path: string;
-    iconPath?: string
+    iconPath?: string;
 }
 
 const HeaderButton = ({path, button_text, iconPath}: Props) => {
-    const navigate = useNavigate()
-
+    const navigate = useNavigate();
     path = path.toLowerCase();
-
-    let active = "";
+    let active = '';
 
     if (window.location.pathname.toLowerCase().includes(path)) {
-        active = " header__link_active";
+        active = 'header__link_active';
     }
 
-    if (window.location.pathname.toLowerCase() !== "/" && path === "/") {
-        active = ""
+    if (window.location.pathname.toLowerCase() !== locations.rootPath && path === locations.rootPath) {
+        active = '';
     }
 
     return (
-        <div onClick={() => navigate(path)}
-             className={"header__link" + active}>
-            {iconPath && <img src={iconPath} alt={iconPath}></img>}
+        <div onClick={() => navigate(path)} className={`header__link ${active}`}>
+            {iconPath && <img src={iconPath} alt={iconPath}/>}
             {button_text}
         </div>
     );
