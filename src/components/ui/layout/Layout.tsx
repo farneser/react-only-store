69c68a9c848cdacb897cf4717dab564ac32d5a9f
farneser <farneser1@gmail.com>
2023-10-14 11:22:41 +0300

    feat: layout styles

--- src/components/ui/layout/Layout.tsx
+++ src/components/ui/layout/Layout.tsx
@@ -1,15 +1,17 @@
-import {Outlet} from "react-router-dom";
-import {FC} from "react";
-import Header from "./header/Header";
-import Footer from "./footer/Footer";
+import {Outlet} from 'react-router-dom';
+import {FC} from 'react';
+import Header from './header/Header';
+import Footer from './footer/Footer';
+
+import './layout.scss';
 
 const Layout: FC = () => {
     return (
-        <div id="layout">
+        <div id="layout" className={'layout'}>
             <Header/>
             <main>
-                <div id="body">
-                    <div className="body__container">
+                <div id="body" className={'body'}>
+                    <div className={'body__container'}>
                         <Outlet/>
                     </div>
                 </div>
