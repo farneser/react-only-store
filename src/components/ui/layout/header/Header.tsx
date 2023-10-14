4692ecf482fa6e57940c9fe7c81d0c43b331327f
farneser <farneser1@gmail.com>
2023-10-14 11:22:34 +0300

    feat: header styles

--- src/components/ui/layout/header/Header.tsx
+++ src/components/ui/layout/header/Header.tsx
@@ -1,7 +1,8 @@
 import React, {useContext} from 'react';
-import {CartContext} from "../../../providers/CartProvider";
-import HeaderButton from "./button/HeaderButton";
-import locations from "../../../../locations";
+import {CartContext} from '../../../providers/CartProvider';
+import HeaderButton from './button/HeaderButton';
+import locations from '../../../../locations';
+import './header.scss';
 
 const Header: React.FC = () => {
     const context = useContext(CartContext);
@@ -9,18 +10,16 @@ const Header: React.FC = () => {
     let sum = 0;
 
     if (context) {
-        context.cart.map(c => {
+        context.cart.map((c) => {
             sum += c.count * c.product.price;
-        })
+        });
     }
 
     return (
         <div id="header">
-            <HeaderButton button_text={"Home"} path={locations.rootPath}/>
-            <HeaderButton button_text={"Cart"} path={locations.cartPath}/>
-            <div>
-                <span>{sum}</span>
-            </div>
+            <HeaderButton button_text="Home" path={locations.rootPath}/>
+            <HeaderButton button_text="Cart" path={locations.cartPath}/>
+            <span className="cart-total">({sum})</span>
         </div>
     );
 };
