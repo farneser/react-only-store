cbabc009cd23c42bc4b4e5fcefb01ed0f65c91e7
farneser <farneser1@gmail.com>
2023-10-14 11:23:39 +0300

    feat: three-step form

--- src/components/ui/emptyList/EmptyList.tsx
+++ src/components/ui/emptyList/EmptyList.tsx
@@ -1,7 +1,7 @@
 import {FC} from "react";
 import {Link, To} from "react-router-dom";
 
-const EmptyList: FC<{ message: String; link?: { message: String, path: To } }> = ({message, link}) => {
+const EmptyList: FC<{ message: string; link?: { message: string, path: To } }> = ({message, link}) => {
     return <div>
         <p>{message} {link ? <Link to={link.path}>{link.message}</Link> : ""}</p>
     </div>
---
+++ src/components/ui/order/ThreeStepForm.tsx
@@ -0,0 +1,37 @@
+import React, {useContext, useState} from 'react';
+import AddressStep from './AddressStep';
+import CardDataStep from './CardDataStep';
+import ConfirmStep from './ConfirmStep';
+import './threeStepForm.scss';
+import {CartContext} from "../../providers/CartProvider";
+
+const ThreeStepForm: React.FC = () => {
+    const context = useContext(CartContext)
+
+    const [step, setStep] = useState(1);
+
+    const handleNext = () => {
+        setStep(step + 1);
+    };
+
+    const handlePrevious = () => {
+        setStep(step - 1);
+    };
+
+    const handleSubmit = () => {
+        if (context) {
+            context.cart = []
+        }
+
+    };
+
+    return (
+        <div className={"container"}>
+            {step === 1 && <AddressStep onNext={handleNext}/>}
+            {step === 2 && <CardDataStep onPrevious={handlePrevious} onNext={handleNext}/>}
+            {step === 3 && <ConfirmStep onPrevious={handlePrevious} onSubmit={handleSubmit}/>}
+        </div>
+    );
+};
+
+export default ThreeStepForm;
